752989db85e86a421aa5c21a538bf8d5603d710b
Gary <garyallen@notmyemail.com>
2025-04-24 14:29:28 -0400

    Initial commit: setup Spring Boot and PostgreSQL

---
+++ src/main/java/com/gary/fintechtrack/FinTechTrackApplication.java
@@ -0,0 +1,13 @@
+package com.gary.fintechtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTechTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTechTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gary/fintechtrack/FinTechTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.fintechtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTechTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
